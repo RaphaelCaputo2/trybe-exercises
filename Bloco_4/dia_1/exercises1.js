00ce37d6c819c754b57a62d5f37c5f75a794cc9f
Raphael Caputo <faelelf2@gmail.com>
2020-08-28 01:58:31 -0300

    Adicionando primeiros exercicios

---
+++ Bloco_4/dia_1/exercises1.js
@@ -0,0 +1,11 @@
+let a = 15;
+let b = 10;
+let adicao = a + b;
+let subtracao = a -b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+if (a > b) {
+console.log("Sim é maior");
+}

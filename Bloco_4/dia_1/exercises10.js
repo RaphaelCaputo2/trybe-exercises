1752f51415e4d564af3fc4f95e6e42502d5eb7dc
Raphael Caputo <faelelf2@gmail.com>
2020-08-28 03:01:44 -0300

    Todos os exercicios em javaScript

---
+++ Bloco_4/dia_1/exercises10.js
@@ -0,0 +1,18 @@
+// //Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%, que deve ser considerado no cálculo do lucro.
+// Seu programa também deve emitir uma mensagem de erro e encerrar, caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+
+let custoDoProduto = 2;
+let valorDeVenda = 6;
+
+if (custoDoProduto >= 0 && valorDeVenda >= 0) {
+  let custoDoProduto = custoDoProduto * 1.2;
+  let total = (valorDeVenda - custoDoProduto) * 1000;
+  console.log(total);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};

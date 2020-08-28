e519d863ae9e146e333a36af134d90b7249e1e7a
Raphael Caputo <faelelf2@gmail.com>
2020-08-28 02:20:57 -0300

    Exercises 4

---
+++ Bloco_4/dia_1/exercises4.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 5;
+let c = 15;
+
+ if ((a + c) % 2) {
+    console.log ("positivo")
+} else if ((a - c) % -5) {
+   console.log ("Negative")
+} else {
+  console.log("Zero")
+}
+
+
+//gabarito
+
+
+
+// let number = 4;
+
+// if (number > 0) {
+//   console.log('positive');
+// } else if (number < 0) {
+//   console.log('negative');
+// } else {
+//   console.log('zero');
+// };

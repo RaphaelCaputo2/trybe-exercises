3b1541383a6d33fcb3adf688bddfd1c62250280d
Raphael Caputo <faelelf2@gmail.com>
2020-08-28 18:56:41 -0300

    Exercicio do video primeiro conteudo

---
+++ Bloco_4/dia__2/entendendoArrays.js
@@ -0,0 +1,13 @@
+let nome=[9, 1, 4, 2, 5]
+
+
+
+for(let pos=0; pos < nome.length; pos++){
+  nome.sort()
+  console.log(`a posição ${pos} tem o valor ${nome[pos]}`)
+  
+}
+
+
+
+
--- Bloco_4/dia__2/exercises1.js
+++
@@ -1,2 +0,0 @@
-let nome = ["raphael", "flavia", "meg"]
-console.log(nome);

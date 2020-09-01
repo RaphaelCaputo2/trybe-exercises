15b495990bdd74ccc975c1d73aca60fa8ccbd882
Raphael Caputo <faelelf2@gmail.com>
2020-09-01 19:48:22 -0300

    Adicionando exercicio 3

---
+++ Bloco_4/dia_4/aboutFunction3.js
@@ -0,0 +1,13 @@
+function numeros(arrayInteiro) {
+    let numeros = 0;
+    for (let i in arrayInteiro) {
+        if (arrayInteiro[numeros] > arrayInteiro[i]) {
+            numeros = i;
+
+        }
+
+    }
+    return numeros;
+}
+
+console.log(numeros([2, 4, 6, 7, 10, 0, -3]));

835ff6aef628cdb275fd4836c72b3a4b4db906a6
Raphael Caputo <faelelf2@gmail.com>
2020-09-02 02:15:04 -0300

    Adicionando exercicio 5 e 6

--- Bloco_4/dia_4/aboutFunction5.js
+++ Bloco_4/dia_4/aboutFunction5.js
@@ -1,13 +1,25 @@
-function numeros(arrayInteiro) {
-    let numeros = 0;
-    for (let i in arrayInteiro) {
-        if (arrayInteiro.length == arrayInteiro[i].length) {
-            numeros = arrayInteiro[i];
+ //exercicio feito pelo gabarito, para estudos!!!!
 
+
+function maisRepetido(numeros) {
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+    for (let index in numeros) {
+      let verificaNumero = numeros[index];
+      for (let index2 in numeros) {
+        if (verificaNumero === numeros[index2]) {
+          contNumero++;
         }
-
+      }
+      if (contNumero > contRepetido) {
+        contRepetido = contNumero;
+        indexNumeroRepetido = index;
+      }
+      contNumero = 0;
     }
-    return numeros;
-}
+    return numeros[indexNumeroRepetido];
+  }
+  console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
 
-console.log(numeros([2, 3, 2, 5, 8, 2, 3]));
+ 

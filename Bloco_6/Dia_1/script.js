be0b483960f572c73d8c3342a9b757741649c645
Raphael Caputo <faelelf2@gmail.com>
2020-09-17 20:52:33 -0300

    Exercicios inacabados

---
+++ Bloco_6/Dia_1/script.js
@@ -0,0 +1,38 @@
+const estados2 = document.getElementById('comboBox');
+const estados = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+}
+const soma = estados2 + estados;
+estados2.addEventListener('click', function (e) {
+  let criarTag = document.createElement('option');
+    for(let key in estados) {
+    
+    }
+   
+})

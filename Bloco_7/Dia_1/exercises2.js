be66beabdd1fe0b8d44538360ecc7fc74aeb52f3
Raphael Caputo <faelelf2@gmail.com>
2020-09-29 01:05:21 -0300

    exercises 2

---
+++ Bloco_7/Dia_1/exercises2.js
@@ -0,0 +1,15 @@
+const longestWord = text => {
+  let letrasArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+  for(let i of letrasArray) {
+    if(i.length > maxLength) {
+      maxLength = i.length
+      result = i;
+      
+    }
+  }
+  console.log(result);
+
+}
+ longestWord("o peito do pe do pedro é preto") //

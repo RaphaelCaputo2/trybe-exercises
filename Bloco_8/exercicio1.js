0470dad16b00ce9951d055438ee981167af9f5bb
Raphael Caputo <faelelf2@gmail.com>
2020-10-06 02:06:44 -0300

    Mandando mais exercicios

--- Bloco_8/exercicio1.js
+++ Bloco_8/exercicio1.js
@@ -1,13 +1,19 @@
 let nomes = [{
   nome: 'Raphael',
-  sobrenome: 'Caputo'
+  sobrenome: 'Caputo',
+  anoNascimento: 1988
 },
 {
   nome: 'Vera',
-  sobrenome: 'Alice'
+  sobrenome: 'Alice',
+  anoNascimento: 1959
 }];
 
 nomes.forEach((parameter1) => {
-console.log(parameter1.nome);
-  return nomes;
+  if (parameter1.anoNascimento === 1959 || parameter1.nome === 'Vera') {
+    console.log('Parabens conseguimos');
+  }
+ 
+return nomes;
+
 })

b07a1b08a9bd1a3d7d28c9ff94d9c5d94884cb9c
Raphael Caputo <faelelf2@gmail.com>
2020-10-06 01:42:08 -0300

    Adicionando exercicios ao dia 2

---
+++ Bloco_8/exercicio1.js
@@ -0,0 +1,13 @@
+let nomes = [{
+  nome: 'Raphael',
+  sobrenome: 'Caputo'
+},
+{
+  nome: 'Vera',
+  sobrenome: 'Alice'
+}];
+
+nomes.forEach((parameter1) => {
+console.log(parameter1.nome);
+  return nomes;
+})

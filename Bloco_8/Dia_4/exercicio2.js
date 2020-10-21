19344201f7e8d435698a1854b51e240b8ae81d63
Raphael Caputo <faelelf2@gmail.com>
2020-10-21 19:00:20 -0300

    add pasta blobo 9

--- Bloco_8/Dia_4/exercicio2.js
+++ Bloco_8/Dia_4/exercicio2.js
@@ -1,3 +1,14 @@
-const array = [1, 2, 3];
-console.log(...array);
-console.log(...array);
+const assert = require('assert')
+
+// escreva sum abaixo
+const sum = (...numbers) => {
+  let total = 0
+  for (number of numbers) total += number
+  return total
+ }
+assert.equal(sum(0), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
+

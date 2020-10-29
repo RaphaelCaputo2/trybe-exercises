88a8213dd6a67da4c502cc9eb3b56b3331cf67fe
Raphael Caputo <faelelf2@gmail.com>
2020-10-29 18:35:08 -0300

    Organizacao de pastas

--- Bloco_10/newObj.test.js
+++ Bloco_10/newObj.test.js
@@ -1,21 +1,10 @@
-const { test, expect } = require("@jest/globals");
+const { describe } = require('yargs');
+const objetos = require('./newObj');
 
-const obj1 = {
-  city: "São Paulo",
-  state: "São Paulo"
-};
 
-const obj2 = {
-  city: "Campinas",
-  state: "São Paulo"
-};
-
-const obj3 = {
-  city: "São Paulo",
-  state: "São Paulo"
-};
-
+describe('objetos', () => {
 test('comparing the objects is equal', () => {
   expect(obj3).toEqual(obj1);
   expect(obj3).not.toEqual(obj2);
 })
+});

475c47e40346598113ac74b4090b865c3b2670dd
Raphael Caputo <faelelf2@gmail.com>
2020-10-28 22:41:19 -0300

    Fazendo testes na funcao myFizzBuzz

--- Bloco_10/myFizzBuzz.test.js
+++ Bloco_10/myFizzBuzz.test.js
@@ -1,2 +1,17 @@
-const myFizBuzz = require('./myFizzBuzz');
+const { expect, test } = require('@jest/globals');
+const myFizzBuzz = require('./myFizzBuzz');
 
+describe('myFizzBuzz', () => {
+  test('number rest 0 for 3 and 5', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+  })
+  test('number for 3 rest 0', () => {
+    expect(myFizzBuzz(3)).toEqual('fizz');
+  })
+  test('number for 5 rest 0', () => {
+    expect(myFizzBuzz(5)).toEqual('buzz')
+  })
+  test('not a number for 3 or 5', () => {
+    expect(myFizzBuzz('7')).toBeFalsy();
+  })
+})

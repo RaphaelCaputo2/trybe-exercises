ee1270c36b633c3686aa2b792d5b88a9c05ecc20
Raphael Caputo <faelelf2@gmail.com>
2020-10-30 20:37:22 -0300

    iniciando exercicio 5

---
+++ Bloco_10/Dia_3/exercicio5.js
@@ -0,0 +1,9 @@
+let firstFunction = str => {
+  return str.toUpperCase();
+};
+let secondFunction = str => {
+  return str.toUpperCase();
+};
+let thirdFunction = str => {
+  return str.toUpperCase();
+};

c5d6fa9df22a3330cbd412d35649d926a650623a
Raphael Caputo <faelelf2@gmail.com>
2020-11-08 23:25:30 -0300

    Finalizando

---
+++ Front_End/Bloco_11/Dia_1/firstday/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Skills from './Skills.js'
+import Skill2 from './Skill2.js'
+function App() {
+  return (
+    <div>
+  <Skills/>
+  <Skill2/>
+  </div>
+  );
+}
+
+export default App;
---
+++ Front_End/Bloco_11/Dia_1/firstday/src/Skill2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const msg = 'Boaaa';
+
+class Skill2 extends React.Component {
+  render () {
+    return (<h1>{msg}</h1>)
+  }
+
+
+}
+export default Skill2;

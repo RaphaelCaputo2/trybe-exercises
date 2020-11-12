930109966992edad5996f998570d128df44ecb1c
Raphael Caputo <faelelf2@gmail.com>
2020-11-12 14:57:53 -0300

    Enviando mais exercicios de react

---
+++ Front_End/Bloco_12/Dia_1/usando-state-react/src/ButtonAlert.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+function handleClick () {
+  alert('Clicou');
+}
+
+class ButtonAlert extends Component {
+  render() {
+    return ()
+      
+  }
+}
+
+
+export default ButtonAlert;

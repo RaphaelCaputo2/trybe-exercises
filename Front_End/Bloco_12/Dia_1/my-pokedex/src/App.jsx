605a1784f7191da2d4de3cbdb4f34c0d1dc43b11
Raphael Caputo <faelelf2@gmail.com>
2020-11-15 01:46:07 -0300

    Exercicio Refatorado com state

--- Front_End/Bloco_12/Dia_1/my-pokedex/src/App.jsx
+++ Front_End/Bloco_12/Dia_1/my-pokedex/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import PokemonList from './PokemonList';
+import Pokedex from './Pokedex';
 
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Pokedex do Caputo</h1>
-      <PokemonList pokemons={pokemons} />
+      <Pokedex pokemons={pokemons} />
     </div>
   )
 }

8264cf6607b9586371ea607f4ea80d776d6706ed
Raphael Caputo <faelelf2@gmail.com>
2020-12-16 19:24:49 -0300

    Adicionando reducers

--- Front_End/Bloco__16_Redux/Redux_Dia_2/player-react-redux/src/App.js
+++ Front_End/Bloco__16_Redux/Redux_Dia_2/player-react-redux/src/App.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Siderbar from './components/Sidebar';
 import Player from './components/Player';
-import categories from './data'
+
 import store from './store';
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      myCategories: [...categories],
-    }
-  }
+ 
   render() {
     const { myCategories } = this.state
     return (

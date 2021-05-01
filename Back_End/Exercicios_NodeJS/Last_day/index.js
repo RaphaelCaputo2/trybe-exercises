efb5d6987066a875cdac29c5cc1b3f8ba4cc9bd3
Raphael Caputo <faelelf2@gmail.com>
2021-05-01 03:20:35 -0300

    Finalizando mais um crud

--- Back_End/Exercicios_NodeJS/Last_day/index.js
+++ Back_End/Exercicios_NodeJS/Last_day/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const simpsons = require('./routes/simpsons');
+const {errorMiddleware, logMiddleware} = require('./mmiddlewares/index');
 
 
 app.get('/', (req, res) => {
   res.send({
     nome: "Raphael"
   })
-})
+});
 
+app.use(express.json());
+app.use(logMiddleware)
+app.use(simpsons);
+
+app.use(errorMiddleware)
 app.listen(port, () => {
   console.log('Servidor rodando na porta 3000')
 })
